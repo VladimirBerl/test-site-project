b97cc55f7a0b816e48dcb29aa255d7adf55d832f
VladimirBerl <vova2002.13lol@gmail.com>
2024-08-12 22:49:41 +0300

    update 2.0

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
-const toggleButton = document.getElementById("navbar-toggle");
-const navbar = document.getElementById("navbar");
-const navLinks = navbar.querySelectorAll(".nav-link");
+const html = document.documentElement;
 const body = document.body;
+const header = document.getElementById("header");
+const navbar = document.getElementById("navbar");
+const toggleButton = document.getElementById("navbar-toggle");
+const navLinks = navbar.querySelectorAll(".nav-link");
 
 const toggleModal = () => {
   const isActive = toggleButton.classList.toggle("active");
   navbar.classList.toggle("active", isActive);
-};
-
-const toggleModalOff = () => {
-  toggleButton.classList.remove("active");
-  navbar.classList.remove("active");
+  html.classList.toggle("hidden", isActive);
 };
 
 toggleButton.addEventListener("click", (event) => {
@@ -28,9 +26,64 @@ body.addEventListener("click", (event) => {
 });
 
 navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    toggleModalOff();
+  link.addEventListener("click", (event) => {
+    navLinks.forEach((nav) => nav.classList.remove("active"));
+    event.target.classList.add("active");
+    toggleModal();
   });
+});
+
+let lastScrollTop = 0;
+let isScrolling;
+const onScroll = () => {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  // Проверяем направление скролла
+  if (scrollTop > lastScrollTop) {
+    // Скролл вниз - скрываем хедер
+    header.classList.add("hidden");
+  } else {
+    // Скролл вверх - показываем хедер
+    header.classList.remove("hidden");
+  }
+
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Не позволяем lastScrollTop становиться отрицательным
+};
+window.addEventListener("scroll", () => {
+  if (!isScrolling) {
+    isScrolling = true;
+
+    requestAnimationFrame(() => {
+      onScroll();
+      isScrolling = false;
+    });
+  }
+});
+
+// lib
+const swiper = new Swiper(".swiper", {
+  spaceBetween: 20,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  slidesPerView: 1,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    900: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
 });
 
 particlesJS(
